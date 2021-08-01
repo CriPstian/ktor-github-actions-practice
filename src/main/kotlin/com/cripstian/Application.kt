f54a1913cfc3e3988fd898ed193be8363dd1a968
Cristian Popescu <cristian.popescu@esolutions.ro>
2021-08-01 12:44:48 +0300

    fix ktor

--- src/main/kotlin/com/cripstian/Application.kt
+++ src/main/kotlin/com/cripstian/Application.kt
@@ -1,10 +1,10 @@
 package com.cripstian
 
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.jetty.Jetty
 import com.cripstian.plugins.configureRouting
 import com.cripstian.plugins.configureSecurity
 import com.cripstian.plugins.configureSerialization
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.jetty.Jetty
 
 fun main() {
     embeddedServer(Jetty, port = 8080, host = "0.0.0.0") {
